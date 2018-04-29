e96db27cb923315424b87d2caac69da8cbc949af
alex-kampa <aleksander.kampa@gmail.com>
2018-04-30 00:07:33 +0200

    add VaryonTokenTest.sol

---
+++ contracts/VaryonTokenTest.sol
@@ -0,0 +1,31 @@
+contract VaryonTokenTest is VaryonToken {
+
+  /*
+
+  Introduces function setTestTime(uint)
+  
+  Overrides function atNow() to return testTime instead of now()
+
+  */
+
+  uint public testTime = 1;
+  
+  // Events ---------------------------
+
+  event TestTimeSet(uint _now);
+
+  // Functions ------------------------
+
+  constructor() public {}
+
+  function atNow() public constant returns (uint) {
+      return testTime;
+  }
+
+  function setTestTime(uint _t) public onlyOwner {
+    // require( _t > testTime );
+    testTime = _t;
+    emit TestTimeSet(_t);
+  }
+
+}
